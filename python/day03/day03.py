d2d0f9b8587a75e5ce6ac46cf7556dda26f44934
Bert Riffelmacher <sotsoguk@gmail.com>
2021-12-04 02:23:38 +0100

    [Day03] Python

---
+++ python/day03/day03.py
@@ -0,0 +1,89 @@
+import os
+import time
+import cmath
+import bisect
+
+
+def main():
+    # input
+    print(os.getcwd())
+    day = "03"
+    year = "2021"
+    inputFile = f'../inputs/day{day}.txt'
+    start_time = time.time()
+    print(inputFile)
+    part1, part2 = 0, 0
+    with open(inputFile) as f:
+        lines = f.read().splitlines()
+
+    # convert to ints
+    input = [int(l, base=2) for l in lines]
+    num_digits = len(lines[0])
+    epsilon = [0] * num_digits
+    for l in lines:
+
+        for i, b in enumerate(l):
+            # print(i,b)
+            if b == '1':
+                # print("FOUND")
+                epsilon[num_digits-i-1] += 1
+
+    # print(epsilon)
+
+    # print(epsilon)'P
+
+    eps = ""
+    gamma = ""
+    for c in epsilon:
+        if c > (len(lines)//2):
+            eps += "1"
+            gamma += "0"
+        else:
+            eps += "0"
+            gamma += "1"
+    # print(eps[::-1])
+    # print(gamma[::-1])
+    part1 = int(eps[::-1], base=2) * int(gamma[::-1], base=2)
+    # convert commands to complex numbers
+
+    duration = int((time.time() - start_time) * 1000000)
+    inp_sort = sorted(input)
+    # part2
+    n = len(lines[0])
+    to_check, lo_idx, hi_idx= (1<< (n-1)), 0, len(input)
+    while (lo_idx < hi_idx-1):
+        mid = (lo_idx+hi_idx) // 2
+        b = bisect.bisect_left(inp_sort[lo_idx:hi_idx], to_check)
+        if (b+lo_idx) <= mid:
+            lo_idx += b
+        else:
+            hi_idx = lo_idx + b
+            to_check -= (1 << (n-1))
+        n -= 1
+        to_check += (1 << (n-1))
+
+    oxy = inp_sort[lo_idx]
+
+    n = len(lines[0])
+    to_check, lo_idx, hi_idx= (1<< (n-1)), 0, len(input)
+    while (lo_idx < hi_idx-1):
+        mid = (lo_idx+hi_idx) // 2
+        b = bisect.bisect_left(inp_sort[lo_idx:hi_idx], to_check)
+
+        if (b+lo_idx) <= mid:
+            hi_idx = lo_idx+b
+            to_check -= (1 << (n-1))
+        else:
+            lo_idx = lo_idx + b
+        n -= 1
+        to_check += (1 << (n-1))
+
+    co2 = inp_sort[lo_idx]
+
+    part2 = co2 * oxy
+    print(
+        f"AoC {year} - Day {day}:\n\nPart 1:\t{part1}\nPart 2:\t{part2}\nDuration:\t{duration} ns")
+
+
+if __name__ == "__main__":
+    main()
