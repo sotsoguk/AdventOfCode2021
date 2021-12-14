537dcde80d4b17126c73bde4823af6030963fa09
Bert Riffelmacher <sotsoguk@gmail.com>
2021-12-14 10:29:49 +0100

    [Day12] Massive performance improvement

--- python/day12/day12.py
+++ python/day12/day12.py
@@ -4,66 +4,40 @@ import time
 from math import ceil, floor
 from queue import LifoQueue
 from collections import defaultdict
+from collections import deque
 
 
-def recDFS2(node,visited,adj,double_visit):
-    if node == 'end':
-        return 1
-    # print(node,visited)
-    # if node == 'start':
-    #     return 0
-    tmp = 0
-    print(node,visited,double_visit,twice)
-    if double_visit == True:
-        if node.islower():
-            visited.add(node)
-        for v in adj[node]:
-            if v == 'start':
-                continue
-            if v not in visited:
-                tmp +=recDFS2(v,visited.copy(),adj,True,twice)
-    else:
-        if node == 'start':
-            visited.add('start')
-            for v in adj[node]:
-                
-                tmp +=recDFS2(v,visited.copy(),adj,False,[])
+def iterDFS(startNode, adj, part2=False):
+    result = 0
+    p = [[startNode], set(), False]
+    paths = deque()
+    paths.appendleft(p)
+    while paths:
+        nodeList, visitedSet, revisited = paths.popleft()
+        currNode = nodeList[-1]
+        if currNode == 'end':
+            result += 1
         else:
-            if node.islower():
-                visited2 = visited.copy()
-                visited2.add(node)
-            
-            for v in adj[node]:
-                if v == 'start':
-                    continue
-                if v not in visited:
-                    if v == 'start':
-                        continue
-                
-                    tmp += recDFS2(v,visited.copy(),adj,False,)
-                    tmp += recDFS2(v,visited2.copy(),adj,True)
-    return tmp
-def recDFS(node,visited,adj):
-    if node == 'end':
-        return 1
-    # print(node,visited)
-    # if node == 'start':
-    #     return 0
-    if node.islower():
-        visited.add(node)
-    tmp = 0
-    for v in adj[node]:
-        if v == 'start':
-            continue
-        if v not in visited:
-            tmp +=recDFS(v,visited.copy(),adj)
-    return tmp
+            for v in adj[currNode]:
+                if v.islower():
+                    if not v in visitedSet:
+                        paths.appendleft(
+                            [nodeList + [v], visitedSet | {v}, revisited])
+                    else:
+                        if part2 and not revisited:
+                            paths.appendleft(
+                                [nodeList + [v], visitedSet, True])
+                else:
+                    paths.appendleft([nodeList + [v], visitedSet, revisited])
+    return result
+
+
 def main():
     # input
     print(os.getcwd())
     day = "12"
     year = "2021"
-    # inputFile = f'../inputs/day{day}_test2.txt'
+    # inputFile = f'../inputs/day{day}_test3.txt'
     inputFile = f'../inputs/day{day}.txt'
 
     part1, part2 = 0, 0
@@ -73,55 +47,14 @@ def main():
     # generate graph
     adj = defaultdict(list)
     for l in lines:
-        u,v = l.split('-')
+        u, v = l.split('-')
         if v != 'start':
             adj[u].append(v)
         if u != 'start':
             adj[v].append(u)
 
-    print(adj)
-    # visited = set()
-    # visited.add('start')
-    # part1 = recDFS('start',visited,adj)
-    # part2 = recDFS2('start',visited,adj, False,[])
-    # part1
-    p = [['start'],set(),False]
-    paths = []
-    paths.append(p)
-    while paths:
-        nodelist, visset, _ = paths.pop(0)
-        #print(nodelist,visset)
-        currNode = nodelist[-1]
-        if currNode == 'end':
-            part1 +=1
-        else:
-            for v in adj[currNode]:
-                if v.islower():
-                    if not v in visset:
-                        paths.append([nodelist + [v],visset | {v},False])
-                else:
-                    paths.append([nodelist + [v],visset,False])
-
-    # part2 
-    p = [['start'],set(),False]
-    paths = []
-    paths.append(p)
-    while paths:
-        nodelist, visset, revisited = paths.pop(0)
-        #print(nodelist,visset)
-        currNode = nodelist[-1]
-        if currNode == 'end':
-            part2 +=1
-        else:
-            for v in adj[currNode]:
-                if v.islower():
-                    if not v in visset:
-                        paths.append([nodelist + [v],visset | {v},revisited])
-                    else:
-                        if not revisited:
-                            paths.append([nodelist + [v],visset, True])
-                else:
-                    paths.append([nodelist + [v],visset,revisited])
+    part1 = iterDFS('start', adj)
+    part2 = iterDFS('start', adj, True)
     duration = int((time.time() - start_time) * 1000000)
     header = "!" * 20
     print(
