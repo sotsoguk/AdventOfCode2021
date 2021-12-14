8e06e5fee7e342aea22115064a0a457d31fc845b
Bert Riffelmacher <sotsoguk@gmail.com>
2021-12-14 10:51:49 +0100

    [Day13] Cleanup

--- python/day13/day13.py
+++ python/day13/day13.py
@@ -23,31 +23,19 @@ def foldY(dots,axis):
             newDots.append(d)
     return newDots
 def printGrid(grid):
-    minX = 1000
-    minY = 1000
-    maxX = 0
-    maxY = 0
-    for d in grid:
-        minX = min(minX,d[0])
-        minY = min(minY,d[1])
-        maxX = max(maxX,d[0])
-        maxY = max(maxY,d[1])
-    print(minX,minY,maxX,maxY)
-    pgrid = []
+    maxX, maxY = max(grid,key = lambda x:x[0])[0],max(grid,key = lambda x:x[1])[1]
+    printGrid = []
     for r in range(maxY+1):
-        pgrid.append([0]*(maxX+1))
-    print(pgrid)
+        printGrid.append([0]*(maxX+1))
     for d in grid:
-        # print(d[1],d[0])
-        pgrid[d[1]][d[0]] = 1
-    print(pgrid)
-    for r in range(len(pgrid)):
+        printGrid[d[1]][d[0]] = 1
+    for r in range(len(printGrid)):
         l = u''
-        for c in range(len(pgrid[0])):
-            if pgrid[r][c] == 0:
+        for c in range(len(printGrid[0])):
+            if printGrid[r][c] == 0:
                 l+=u' '
             else:
-                l+=u'\u2588'
+                l+=u'#'
         print(l.encode('utf8'))
 def main():
     # input
@@ -61,35 +49,32 @@ def main():
     start_time = time.time()
     part1, part2 = 0,0
     dots = []
-    lnr = 0
     instructions=[]
-    while True:
-        if lines[lnr] != "":
-            a,b = lines[lnr].split(',')
-            dots.append((int(a),int(b)))
-            lnr += 1
-        else:
-            lnr +=1
-            break
-    for i in range(lnr,len(lines)):
-        tmp = lines[i].split()[2]
-        instructions.append((tmp[0],int(tmp[2:])))
     
-    print(dots)
-    for i in instructions:
+    for l in lines[:lines.index("")]:
+        a,b = list(map(int,l.split(',')))
+        dots.append((a,b))
+    
+    for l in lines[lines.index("")+1:]:
+        tmp = l.split()[2]
+        instructions.append((tmp[0],int(tmp[2:])))
+   
+    for c,i in enumerate(instructions):
         if i[0] == 'x':
             dots = foldX(dots,i[1])
         else:
             dots = foldY(dots,i[1])
-        print("!!!! ",i)
-        print(len(set(dots)))
-        # print(dots)
+        
+        if c == 0:
+            part1 = len(set(dots))
+        
     printGrid(dots)
+    print(dots)
     duration = int((time.time() - start_time) * 1000000)
     header = "#" * 20
     print(
         f"{header}\n*AoC {year} - Day {day} *\n{header}\n\nPart 1:\t{part1}\nPart 2:\t{part2}\nTime:\t{duration // 1000} ms")
-    pinrt
+    
 
 if __name__ == "__main__":
     main()
